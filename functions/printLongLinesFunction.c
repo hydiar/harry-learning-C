22cdb6273473942ce4c8759160f8607a1f13a220
Harry Powell <powell.harryj@gmail.com>
2021-05-28 21:53:35 +0100

    Add files via upload

---
+++ functions/printLongLinesFunction.c
@@ -0,0 +1,66 @@
+//written by Harry Powell, Compiled with C99
+//prints the last 10 strings longer than 10 chars each
+#include <stdio.h>
+
+#define MAXLINE 100
+#define MAXSTOREDLINES 10
+#define THRESHOLD 10
+
+
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+int main() {
+	int len;
+	int max;	
+	char line[MAXLINE];
+	
+	char outputlines[MAXLINE][MAXSTOREDLINES];
+	//char totallines[MAXLINE*MAXSTOREDLINES+(MAXLINE-1)];
+	
+	
+	int linestoredcount;
+	int i;
+	linestoredcount = 0;
+	while ((len = getline(line, MAXLINE)) > 0) {
+		if (len > THRESHOLD) {
+			for (i = 0; i < MAXLINE; ++i) {
+				outputlines[i][linestoredcount] = line[i];
+			}
+		++linestoredcount;
+		}
+	}
+	if (linestoredcount > 0) {
+		printf("Printing lines longer than %i:\n", THRESHOLD);
+		for (i = 0; i < MAXSTOREDLINES; ++i) {	
+			int j;
+			char currentprintstring[MAXLINE];
+			for (j = 0; j < MAXLINE; j++) {
+				currentprintstring[j] = outputlines[j][i];
+			}
+			printf("%s\n", currentprintstring);
+		}
+	}	
+	return 0;
+}
+
+int getline(char s[], int lim) {
+	int c, i;
+
+	for (i=0; i<lim-1 && (c=getchar()) != EOF && c!= '\n'; i++)
+		s[i] = c;
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+void copy(char to[], char from[]) {
+	int i = 0;
+
+	while ((to[i] = from[i] != '\0')) {
+		++i;
+	}
+}
