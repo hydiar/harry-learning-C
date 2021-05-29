a5c490a64259580e7d1c9f55e375eb033593a67a
Harry Powell <powell.harryj@gmail.com>
2021-05-29 14:53:20 +0100

    Add files via upload

--- functions/printLongLinesFunction.c
+++ functions/printLongLinesFunction.c
@@ -1,46 +1,45 @@
-//written by Harry Powell, Compiled with C99
-//prints the last 10 strings longer than 10 chars each
 #include <stdio.h>
-
-#define MAXLINE 100
-#define MAXSTOREDLINES 10
+#define MAXLINELEN 100
+#define MAXLINES 100
 #define THRESHOLD 10
 
-
 int getline(char line[], int maxline);
-void copy(char to[], char from[]);
 
 int main() {
 	int len;
-	int max;	
-	char line[MAXLINE];
+	char line[MAXLINELEN];
+	char output[MAXLINELEN][MAXLINES];
 	
-	char outputlines[MAXLINE][MAXSTOREDLINES];
+	int linecount, i;
+	linecount = 0;
 	
-	int linestoredcount;
-	int i;
-	linestoredcount = 0;
-	while ((len = getline(line, MAXLINE)) > 0) {
-		if (len > THRESHOLD) {
-			for (i = 0; i < MAXLINE; ++i) {
-				outputlines[i][linestoredcount] = line[i];
+	printf("Please type an input. Inputs longer than %i will be printed back to you\n", THRESHOLD);
+
+	while ((len = getline(line, MAXLINELEN)) > 1) {
+		if (len > THRESHOLD){
+			for (i = 0; i < len; ++i) {
+				output[linecount][i] = line[i];
 			}
+			++linecount;
 		}
-		++linestoredcount;
 	}
-	if (linestoredcount > 0) {
+	int printcount = 0;
+	if (linecount > 0) {
 		printf("Printing lines longer than %i:\n", THRESHOLD);
-		for (i = 0; i < linestoredcount; ++i) {	
-			printf("%s", outputlines[i]);
+		while (printcount < linecount) {
+			printf("%s", output[printcount]);
+			++printcount;
 		}
-	}	
+	}
+	else
+		printf("There were no lines longer than %i:\n", THRESHOLD);
 	return 0;
 }
 
 int getline(char s[], int lim) {
 	int c, i;
 
-	for (i=0; i<lim-1 && (c=getchar()) != EOF && c!= '\n'; i++)
+	for (i=0; i<lim-1 && (c=getchar()) != EOF && c!= '\n'; ++i)
 		s[i] = c;
 	if (c == '\n') {
 		s[i] = c;
@@ -48,12 +47,4 @@ int getline(char s[], int lim) {
 	}
 	s[i] = '\0';
 	return i;
-}
-
-void copy(char to[], char from[]) {
-	int i = 0;
-
-	while ((to[i] = from[i] != '\0')) {
-		++i;
-	}
 }
